72ead06e763b5e1f2c25d09d124dd638e25b4114
Antonio <rodrigowserra@gmail.com>
2023-10-17 16:30:28 +0100

    add atributos

--- src/main/java/com/basic/atributos/protegido/Cachorro.java
+++ src/main/java/com/basic/atributos/protegido/Cachorro.java
@@ -3,7 +3,7 @@ package com.basic.atributos.protegido;
 public class Cachorro extends Animal {
 
     public void setNome(String novoNome) {
-        super.nome = novoNome;// podemos acessar o atributo protegido 'nome' aqui
+        super.nome = novoNome; // podemos acessar o atributo protegido 'nome' aqui
     }
 
 }
