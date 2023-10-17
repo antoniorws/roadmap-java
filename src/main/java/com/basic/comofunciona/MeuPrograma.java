b9764f83abaa5adde10a3d8f650cc2b3e3135591
Antonio <antonioserra@chinasystems.com>
2023-10-17 16:03:55 +0100

    Add Code - Como o java funciona

---
+++ src/main/java/com/basic/comofunciona/MeuPrograma.java
@@ -0,0 +1,13 @@
+package com.basic.comofunciona;
+
+public class MeuPrograma {
+
+    private static void cumprimenta() {
+        System.out.println("Olá, mundo!");
+    }
+
+    public static void main(String[] args) {
+        cumprimenta();
+    }
+
+}
