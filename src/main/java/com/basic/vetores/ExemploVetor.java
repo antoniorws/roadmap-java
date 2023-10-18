6c5104c0aa242ff4cdaf27d73dc870e50cccb712
Antonio <rodrigowserra@gmail.com>
2023-10-18 14:54:10 +0100

    Add vetores

---
+++ src/main/java/com/basic/vetores/ExemploVetor.java
@@ -0,0 +1,19 @@
+package com.basic.vetores;
+
+public class ExemploVetor {
+    public static void main(String[] args) {
+        // Declarar e inicializar um vetor de inteiros
+        int[] numeros = new int[5];
+
+        // Atribuir valores aos elementos do vetor
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+
+        // Acessar e imprimir elementos do vetor
+        System.out.println("O primeiro número é: " + numeros[0]);
+        System.out.println("O terceiro número é: " + numeros[2]);
+    }
+}
