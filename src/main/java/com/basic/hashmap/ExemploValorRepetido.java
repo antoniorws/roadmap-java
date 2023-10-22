bcd29f246fc7b61aa4d8f1b2f67ba4a70081a276
Antonio <rodrigowserra@gmail.com>
2023-10-22 15:20:18 +0100

    add mais exemplos com hashMap

---
+++ src/main/java/com/basic/hashmap/ExemploValorRepetido.java
@@ -0,0 +1,19 @@
+package com.basic.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExemploValorRepetido {
+    public static void main(String[] args) {
+        Map<String, Integer> mapaDeIdades = new HashMap<>();
+
+        // Fazendo put da mesma chave duas vezes mas com values diferentes
+        mapaDeIdades.put("Alice", 25);
+        mapaDeIdades.put("Alice", 2);
+
+        for (int idade : mapaDeIdades.values()) {
+            // O útltimo value é o que prevalece
+            System.out.println(idade); // saída: 2
+        }
+    }
+}
